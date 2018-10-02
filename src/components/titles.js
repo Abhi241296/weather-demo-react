78d1920ccb9c2d36737385b5ff05874091979bbe
Abhi241296 <abhishekhaksar35@gmail.com>
2018-10-01 22:14:45 -0700

    few ui changes

--- src/components/titles.js
+++ src/components/titles.js
@@ -4,14 +4,7 @@ class Titles extends React.Component {
 
     render() {
 
-        return (
-
-            <div>
-
-                <h1 className="title-container__title">Simple Weather App </h1>
-                <p className="title-container__subtitle"> Monitoring weather conditions...  </p>
-            </div>
-        )
+        return null
     }
 }
 
